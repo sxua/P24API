618150e39a5f1c1ac8260d998fe89a75e559068f
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2011-06-28 14:17:49 +0300

    initial commit

---
+++ README.markdown
@@ -0,0 +1,13 @@
+Ruby wrapper for Privat24 API
+==================================================================
+
+...
+
+```ruby
+load 'p24api.rb'
+p24 = P24.new # for PRP operations
+
+p24.exchange({:coursid => 3}) # get exchange rates from NBU
+```
+
+...
---
+++ p24api.rb
@@ -0,0 +1,63 @@
+require 'base64'
+require 'cgi'
+require 'digest/md5'
+require 'digest/sha1'
+require 'net/https'
+
+class P24
+  attr_reader :response
+  
+  def initialize(*args)
+    @url = URI.parse('https://privat24.privatbank.ua/p24/accountorder?oper=prp&PUREXML')
+    unless args.empty?
+      @merchant_id, @password, @url = args[0], args[1], (URI.parse(args[2]) rescue @url)
+    else
+    end
+  end
+  
+  def self.define_methods(type,*methods)
+    methods.each do |method|
+      define_method method do |*args|
+        @params = [method.to_s]
+        args.first.each {|k,v| @params << "#{k}=#{v}"} if args.first.is_a?(Hash)
+        query(type,@params)
+      end
+    end
+  end
+  
+  define_methods 'get', :exchange, :apicour, :deposit, :atm, :pboffice, :avias, :bonus, :kontech, :konauto, :konbez, :wifi
+  # define_methods 'post', :privat24, :liqpay
+  
+  private
+  
+  def query(type,data=nil)
+    connection = Net::HTTP.new(@url.host,@url.port)
+    connection.use_ssl = true
+    connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    connection.start do |http|
+      url = type == 'post' ? @url.request_uri : @url.request_uri + '&' + (data.is_a?(Array) ? data.join('&') : (data.is_a?(String) ? data : ''))
+      @response = http.send(type,url,(data unless type == 'get')).body if ['get','post'].include?(type)
+    end
+    @response
+  end
+  
+  # def signature(data)
+  #   sha1(md5(data + @password))
+  # end
+  # 
+  # def liqpay_signature(merch_sign,xml)
+  #   Base64.encode64(sha1(merch_sign + xml + merch_sign))
+  # end
+  # 
+  # def rawurlencode(str)
+  #   CGI.escape(str).gsub('+','%20')
+  # end
+  # 
+  # def sha1(str)
+  #   Digest::SHA1.hexdigest(str)
+  # end
+  # 
+  # def md5(str)
+  #   Digest::MD5.hexdigest(str)
+  # end
+end
